ce7ea0ed357d795ab83232152beb9329484bcc1d
Eric Do <ericdo.617@gmail.com>
2017-10-07 08:57:54 -0700

    Added call to Wiki API

--- src/WikiArticleRow.js
+++ src/WikiArticleRow.js
@@ -3,12 +3,15 @@ import React, { Component } from 'react';
 class WikiArticleRow extends React.Component {
     render() {
       const article = this.props.article;
-      const title = article.title;
-      const description = article.description;
+      const title = this.props.name;
+      const description = this.props.description;
+      const url = this.props.url;
+
       return (
         <li>
           <h1>{title}</h1>
           <p>{description}</p>
+          <p>{url}</p>
         </li>
       );
     }
--- src/WikiSearchResults.js
+++ src/WikiSearchResults.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
 import WikiArticleRow from './WikiArticleRow';
+import $ from 'jquery'; 
 
 class WikiSearchResults extends React.Component {
-    render() {
-      const rows = [];
-      
-      this.props.articles.forEach((article) => {      
-        rows.push(
-          <WikiArticleRow
-            article={article}
-            key={article.title} />
-        );
-      });
-  
-      return (
-        <ul>{rows}</ul>
-      );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {articles : []};
   }
+
+  componentDidMount() {
+    this.WikiSearchResults();
+  }
+
+WikiSearchResults() {
+  $.getJSON('https://en.wikipedia.org/w/api.php?action=opensearch&search=netflix&limit=10&namespace=0&format=json')
+  //.then(({ results }) => this.setState({ articles: results }));
+  .then((data) => {
+    this.setState({articles: data});
+    console.log(this.state.articles);
+  });
+    
+}
+
+  render() {
+    const rows = [];
+    var nameAry = this.state.articles[1];
+    var descriptionAry = this.state.articles[2];
+    var urlAry = this.state.articles[3];
+
+    console.log("Articles: " + nameAry);
+    
+    /* debugger;
+    console.log(nameAry.length); */
+
+    for (var i = 0; i < this.state.articles.length; i++) {
+      //console.log("My name is: " + nameAry[i]);
+      rows.push(
+        <WikiArticleRow
+          name = { nameAry[i] }
+          description = { descriptionAry[i]}
+          url = { urlAry[i] }
+          key = { urlAry[i] }
+           />
+      )
+    }
+    
+    /* this.props.articles.forEach((article) => {      
+      rows.push(
+        <WikiArticleRow
+          article={article}
+          key={article.title} />
+      );
+    }); */
+
+    return (
+      <ul>{rows}</ul>
+    );
+  }
+}
 
   export default WikiSearchResults;
